a0d24cee5d55a5438b745323c9228786b38d7978
kevin rossi <kevinrossi@free.fr>
2024-09-16 22:18:09 +0200

    added validations and associations + name group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,6 @@
 class Group < ApplicationRecord
+  has_many :users, through: :group_users
+  validates :name, presence: true
+  validates :city, presence: true
+  validates :description, presence: true, length: { minimum: 10 }
 end
