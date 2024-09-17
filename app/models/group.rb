89361fb3dfcd31695fb71c8043a7c4ad21376ff6
kevin rossi <kevinrossi@free.fr>
2024-09-17 17:16:12 +0200

    added geocoder mapbox and search bar in page home

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,6 @@
 class Group < ApplicationRecord
+  geocoded_by :city
+  after_validation :geocode, if: :will_save_change_to_city?
   has_many :users, through: :group_users
   has_one_attached :photo
   validates :name, presence: true
