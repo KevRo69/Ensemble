bbf82523857afd506b113278e30974ab61e1fbbe
kevin rossi <kevinrossi@free.fr>
2024-09-17 11:09:48 +0200

    added photo and bio to users form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
   has_many :groups, through: :group_users
+  has_one_attached :photo
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :age, presence: true
