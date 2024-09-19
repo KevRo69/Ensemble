15afb28ff98078171398c536db3255f329fd1668
kevin rossi <kevinrossi@free.fr>
2024-09-19 16:53:37 +0200

    french devise and pages in french

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :age, :address, :nickname, :phone_number, :bio])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo, :age, :address, :nickname, :phone_number, :bio])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :age, :address, :nickname, :phone_number, :bio])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :photo, :age, :address, :nickname, :phone_number, :bio])
   end
 end
